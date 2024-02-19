b894e8517535bc34316e6732bfe366d5b34d5e81
kjacks <kjack227@gmail.com>
2024-02-19 21:41:45 +0100

    Updates

--- zvezoskop/_app/immutable/chunks/paths.475e6c6a.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1roagyh)==null?void 0:s.base)??"/zvezoskop";var a;const o=((a=globalThis.__sveltekit_1roagyh)==null?void 0:a.assets)??e;export{o as a,e as b};
